ee6ac5aa77b7181f2709b1b391f7b6d9fbcae73c
cortezd334 <cortezd334@gmail.com>
2021-02-07 20:52:33 -0800

    bills controller

--- backend/app/controllers/bills_controller.rb
+++ backend/app/controllers/bills_controller.rb
@@ -1,2 +1,33 @@
 class BillsController < ApplicationController
+
+    def index
+        bills = Bill.all
+        render json: bills
+    end
+
+    def show
+        bill = Bill.find(params[:id])
+        render json: bill
+    end
+
+    def create
+        bill = Bill.create(bill_params)
+        render json: bill
+    end
+
+    def update
+        bill = Bill.find(params[:id])
+        bill.update(bill_params)
+        render json: bill
+    end
+
+    def delete
+        bill = Bill.find(params[:id])
+        bill.destroy
+        render json: {error: 'Bill has been deleted'}
+    end
+
+    private
+    def bill_params
+        params.require(:bill).permit(:id, :name, :frequency, :amount, :due_date)
 end
