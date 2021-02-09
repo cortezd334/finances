0c3630e44e65aa62b836944b70b4c7525faa5ce5
cortezd334 <cortezd334@gmail.com>
2021-02-08 20:04:24 -0800

    fetch bills

--- frontend/src/bills/DisplayBills.jsx
+++ frontend/src/bills/DisplayBills.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { fetchBills } from '../api'
 
 export default function DisplayBills() {
-    return (
-      <Container>
-          <Row>
-              <Col><h3>Name</h3></Col>
-              <Col><h3>Amount</h3></Col>
-              <Col><h3>Due</h3></Col>
-              <Col><h3>Frequency</h3></Col>
-              <Col><h3></h3></Col>
-              <Col><h3></h3></Col>
-          </Row>
-      </Container>
-    );
-  }
+
+  
+  fetchBills()
+  .then(console.log)
+
+  return (
+    <Container>
+        <Row>
+            <Col><h3>Name</h3></Col>
+            <Col><h3>Amount</h3></Col>
+            <Col><h3>Due</h3></Col>
+            <Col><h3>Frequency</h3></Col>
+            <Col></Col>
+            <Col></Col>
+        </Row>
+    </Container>
+  );
+}
